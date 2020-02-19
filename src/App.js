cedd5cdf48b5fcca36757f716c554f6fb6427800
Elena <anel92r@gmail.com>
2020-02-19 15:10:56 +0100

    updated navbar and added pictures

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import LogoForDarkTheme from "./Components/Logo/logoForDark.svg";
 // import MainPage from "./Pages/MainPage";
-import ButtonBig from "./Components/ButtonBig";
+import ButtonBig from "./Components/ButtonBig.js";
+import Navbar from "./Components/Navbar.js";
 
 function App() {
   return (
     <div className="App">
+      <Navbar img={LogoForDarkTheme} />
       {/* <MainPage /> */}
       <ButtonBig name="more info" />
     </div>
---
+++ src/Components/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  navBackgroundColorWhite: {
+    // backgroundColor: "transparent",
+    backgroundColor: "#2D2D2D"
+  }
+}));
+
+export default function Navbar({ img }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.navBackgroundColorWhite}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <img src={img} />
+          </Typography>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
