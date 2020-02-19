a06645b323f268199127ab1a5527a3fc5c6eb9fc
Mia Yin <me@miayin.com>
2020-02-19 15:03:41 +0100

    added utils(color) and img folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./App.css";
 // import MainPage from "./Pages/MainPage";
-import ButtonBig from "./Components/ButtonBig";
+// import ButtonBig from "./Components/ButtonBig";
+import BeerCard from "./Components/BeerCard";
+import BeerExample from "./Components/Images/BeerExample.png";
 
 function App() {
   return (
     <div className="App">
       {/* <MainPage /> */}
-      <ButtonBig name="more info" />
+      <BeerCard
+        img={BeerExample}
+        name="Imperial Black Belgian Ale"
+        description="The International Phonetic Alphabet is an alphabetic system of phonetic notation based primarily on the Latin alphabet. It was devised by the International Phonetic Association in the late 19th century as a standardized representation of the sounds of spoken language. "
+      />
     </div>
   );
 }
