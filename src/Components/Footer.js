ced532764410a3809e54df5348ae0a2945e1ab88
Maria Recskova <maria.recska@gmail.com>
2020-02-19 14:32:18 +0100

    new changes footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Footer.css";
+import Logo from "./Images/Logo.png";
 //import Contact from "./Pages/Contact";
 //import About from "./Pages/Terms";
-import Logo from "./Images/Logo.png";
 //import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function Footer() {
