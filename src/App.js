7e6bc53d4bca4dc8eea8143d4b9d4c8695454e90
Maria Recskova <maria.recska@gmail.com>
2020-02-19 11:42:40 +0100

    footer changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import MainPage from "./components/MainPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <MainPage />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <Footer />
+      <section>
+        <h3 className="contact">CONTACT US</h3>
+        <h3 className="terms">TERMS AND POLICIES</h3>
+        <p className="footer-text">
+          BEHERE supports the responsible service of alcohol. It is against the
+          law to sell or supply alcohol to, or obtain alcohol on behalf of, a
+          person under the age of 18 years. License number LIQP14124234.
+        </p>
+        <p className="behere">© BEHERE. All rights reserved.</p>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
