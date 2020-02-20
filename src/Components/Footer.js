7aca251016407451930175ab0b8e465eb020ba6e
Maria Recskova <maria.recska@gmail.com>
2020-02-20 10:41:40 +0100

    small improvements

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,12 +22,12 @@ function Footer() {
           <h3 className="contact">CONTACT US</h3>
           <h3 className="terms">TERMS AND POLICIES</h3>
         </div>
-        <p className="footer-text">
+        <div className="footer-text">
           BEHERE supports the responsible service of alcohol. It is against the
           law to sell or supply alcohol to, or obtain alcohol on behalf of, a
           person under the age of 18 years. License number LIQP14124234.
-        </p>
-        <p className="behere">© BEHERE. All rights reserved.</p>
+          <p className="behere">© BEHERE. All rights reserved.</p>
+        </div>
       </div>
     </section>
   );
