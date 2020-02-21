856c813214484fffbb9d7f657334d8fd7cf479ae
Elena <anel92r@gmail.com>
2020-02-21 13:11:19 +0100

    added links and two pages

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,10 +5,18 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import OrderPage from "../Pages/OrderPage";
+import HomePage from "../Pages/MainPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
+  },
+  "& > * + *": {
+    marginLeft: theme.spacing(2)
   },
   menuButton: {
     marginRight: theme.spacing(2)
@@ -22,26 +30,71 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function Navbar({ img }) {
+function Navbar({ img }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const classes = useStyles();
+  const preventDefault = event => event.preventDefault();
 
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.navBackgroundColorWhite}>
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            <img src={img} />
+            <img src={img} alt="img" />
           </Typography>
           <IconButton
             edge="start"
             className={classes.menuButton}
             color="inherit"
             aria-label="menu"
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
           >
             <MenuIcon />
           </IconButton>
+          <Router>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <Switch>
+                <Route path="/home" component={HomePage}>
+                  <MenuItem onClick={handleClose}>Home</MenuItem>
+                </Route>
+                <Route path="/orderpage" component={OrderPage}>
+                  <MenuItem onClick={handleClose}>
+                    <Link href="#" onClick={preventDefault}>
+                      Order Page
+                    </Link>
+                  </MenuItem>
+                </Route>
+                <Route path="/orderpage" component={OrderPage}>
+                  <MenuItem onClick={handleClose}>
+                    <Link href="#" onClick={preventDefault}>
+                      Contact us
+                    </Link>
+                  </MenuItem>
+                </Route>
+              </Switch>
+            </Menu>
+          </Router>
         </Toolbar>
       </AppBar>
     </div>
   );
 }
+
+export default Navbar;
---
+++ src/Pages/OrderPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import FoodPairing from "../Components/FoodPairing.jsx";
+import TipsCard from "../Components/TipsCard.jsx";
+
+class OrderPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <FoodPairing />
+        <TipsCard />
+      </div>
+      // <React.Fragment>
+      //   <FoodPairing />
+      //   <TipsCard />
+      // </React.Fragment>
+    );
+  }
+}
+
+export default OrderPage;
