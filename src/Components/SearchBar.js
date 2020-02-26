030bc08c8d6881ee6cb34758cbb5054c8d13fe70
Maria Recskova <maria.recska@gmail.com>
2020-02-26 15:06:09 +0100

    searchbar css

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
-
+import "./SearchBar.scss"
 
 export default function SearchBar({ search, getSearch, onChange, updateSearch }) {
   return (
     <div>
-      <form onSubmit={getSearch} className="search-form">
-        <input className="input" type="text" placeholder="Type something" value={search} onChange={updateSearch} />
-        <button className="search-button" type="submit">
-          Search
-        </button>
-      </form>
+      <section className="searchbar-container">
+        <form onSubmit={getSearch} className="search-form">
+          <input className="input" type="text" placeholder="Type something" value={search} onChange={updateSearch} />
+          <i class="fas fa-search"></i>
+        </form>
+      </section>
     </div>
   );
 }
