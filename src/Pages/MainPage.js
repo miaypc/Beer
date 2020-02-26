66abcd4405f199156cbda9557b746359ca32a233
Mia Yin <me@miayin.com>
2020-02-26 17:54:42 +0100

    added animation to main page

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -5,17 +5,39 @@ import MainHeader from "../Components/MainHeader.js";
 import BeerOfTheMonth from "../Components/BeerOfTheMonth";
 import BeerExample from "../Components/Images/BeerExample.png";
 
+import styled, { keyframes } from "styled-components";
+import { bounceInDown, bounceInLeft, bounceInRight } from "react-animations";
+
+const bounceInDownAnimation = keyframes`${bounceInDown}`;
+const bounceInLeftAnimation = keyframes`${bounceInLeft}`;
+const bounceInRightAnimation = keyframes`${bounceInRight}`;
+
+const BounceInDownDiv = styled.div`
+  animation: 5s ${bounceInDownAnimation};
+`;
+const BounceInLeftDiv = styled.div`
+  animation: 5s ${bounceInLeftAnimation};
+`;
+const BounceInRrightDiv = styled.div`
+  animation: 5s ${bounceInRightAnimation};
+`;
+
 function MainPage() {
   return (
     <div className="main-page-container">
-      <Navbar theme="dark" />
-      <MainHeader />
-
-      <BeerOfTheMonth
-        img={BeerExample}
-        name="Imperial Black Belgian Ale"
-        description="A widely available, sessionable craft beer style that showcases both malt and hops. Amber lagers are a medium-bodied lager with a toasty or caramel-like malt character."
-      />
+      <BounceInDownDiv>
+        <Navbar theme="dark" />
+      </BounceInDownDiv>
+      <BounceInLeftDiv>
+        <MainHeader />
+      </BounceInLeftDiv>
+      <BounceInRrightDiv>
+        <BeerOfTheMonth
+          img={BeerExample}
+          name="Imperial Black Belgian Ale"
+          description="A widely available, sessionable craft beer style that showcases both malt and hops. Amber lagers are a medium-bodied lager with a toasty or caramel-like malt character."
+        />
+      </BounceInRrightDiv>
     </div>
   );
 }
