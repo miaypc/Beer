5f06eba8f3e95b8e1f3f7bee471e28ae26f1c975
Elena <anel92r@gmail.com>
2020-02-28 17:02:36 +0100

    added backgrount to contact page

--- src/Components/MainHeader.js
+++ src/Components/MainHeader.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./MainHeader.css";
 import Button from "./Button";
 import Colors from "../utils/Colors";
+import { Link } from "react-router-dom";
 
 function MainHeader() {
   return (
@@ -9,12 +10,14 @@ function MainHeader() {
       <h1 className="find-beer-header">Find The Perfect Beer</h1>
 
       <div className="main-header-button">
-        <Button
-          size="xl"
-          color={Colors.mainYellow}
-          textColor={Colors.mainDark}
-          name="Start Now"
-        />
+        <Link to="/search" style={{ textDecoration: "none", color: "#2d2d2d" }}>
+          <Button
+            size="xl"
+            color={Colors.mainYellow}
+            textColor={Colors.mainDark}
+            name="Start Now"
+          />
+        </Link>
       </div>
       <div className="beer-of-the-month-text">Beer Of The Month</div>
     </div>
